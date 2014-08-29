95553011bdee5dbe1734e8284856f8a411f3e33c
diestel@steloj.de <diestel@steloj.de@2fe983b5-ae8e-ec12-dd4c-d1d120d9b827>
2014-08-29 19:21:34 +0000

    uzado de DCG 'teksto' por analizo/kontrolo

--- trunk/kontrolilo.pl
+++ trunk/kontrolilo.pl
@@ -1,35 +1,7 @@
 :-consult('vortaro.pl').
 :-consult('derivado.pl').
 :-consult('vortana.pl').
-
-/**************** helpfunkcioj por legi el dosiero *************/
-
-% cxu litero?
-is_letter(C,C1) :-
-	C >= 97, C =< 122, C1=C.         % 'a'...'z'
-is_letter(C,C1) :-
-	C >= 65, C =< 90, C1 is C-65+97. % 'A'...'Z' -> 'a'...'z'
-is_letter(39,39).                  % apostrofo
-
-% cxu alia signo?
-is_sign(C) :-
-	memberchk(C,",.- "), put(C).
-
-% legi vorton el dosiero
-legu_vorton1(F,[C|R]) :-
-	get0(F,C1), 
-	is_letter(C1,C), !,
-	legu_vorton1(F,R).
-legu_vorton1(_,[]).
-
-legu_vorton2(F,[C|R]) :-
-	get0(F,C1),
-	(
-         is_letter(C1,C) *-> 
-           legu_vorton2(F,R);
-	   fail
-	),!.
-legu_vorton2(_,[]).
+:-consult('legilo.pl').
 
 /********** analizi unuopajn vortoj aw tutajn tekstojn ***************/
 
@@ -37,38 +9,40 @@ legu_vorton2(_,[]).
 analizu_vorton(N) :-
 	not(N = ''),
 	(
-	    vortanalizo_strikta(N,Y), writeq(Y);
-	    vortanalizo_malstrikta(N,Y), write('malstrikte: '), writeq(Y);
-	    write(' ne analizebla: '),writeq(N)
-	),nl,!.
+	    vortanalizo_strikta(N,[V,S]), format('~w (~w)~n',[V,S]);
+	    vortanalizo_malstrikta(N,[V,S]), format('~w (~w) - malstrikte!~n',[V,S]);
+	    format('~w - NE analizebla!~n',[N])
+	),!.
 
 % analizas unuopan vorton kaj redonas gxin se analizebla
 % aw enkrampigas gxin, se ne analizebla
 kontrolu_vorton(N) :-
 	not(N = ''),
 	(
-	    vortanalizo_strikta(N,_), write(N), write(' ');
-	    vortanalizo_malstrikta(N,Y), write(Y), write(' ');
-	    write('['),write(N),write('] ')
+	    vortanalizo_strikta(N,_), write(N);
+	    vortanalizo_malstrikta(N,Y), write(Y);
+	    format('[_~w_]',N)
 	),!.
 
 % analizas tutan tekstodosieron
 analizu_tekston(Txt) :-
-%	legu,
-	open(Txt,read,F),!,
-	repeat,
-  	legu_vorton1(F,X), name(N,X),
-	(analizu_vorton(N);true),
-	at_end_of_stream(F),!,close(F).
+        phrase_from_file(teksto(T),Txt,[encoding(utf8)]),
+        forall(member(M,T),
+          (
+             M = v(V) *->  (analizu_vorton(V);true) ; true
+      %       M = s(S) *->  write(S)
+          )
+        ),!.
 
 % kontrolas tutan tekstodosieron
 kontrolu_tekston(Txt) :-
-%	legu,
-	open(Txt,read,F),!,
-	repeat,
-  	legu_vorton2(F,X), name(N,X),
-	(kontrolu_vorton(N);true),
-	at_end_of_stream(F),!,close(F).
+        phrase_from_file(teksto(T),Txt,[encoding(utf8)]),
+        forall(member(M,T),
+          (
+             M = v(V) *->  (kontrolu_vorton(V);true) ; 
+             M = s(S) *->  write(S)
+          )
+        ),!.
 
 
 
--- trunk/legilo.pl
+++ trunk/legilo.pl
@@ -21,7 +21,17 @@ apostrofo(C) --> [C], { C = 39 }.
 spaco --> (" " ; "\n" ; "\r").
 intersigno(S) --> [C], { memberchk(C, ",:"), char_code(S,C) }.
 finsigno(S) --> [C], { memberchk(C, ".;!?"), char_code(S,C) }.
+tekstofino --> \+ [_].
 
+% literoj, literchenoj
+litero(L) --> minusklo(L); minusklo_x(L); majusklo(L); majusklo_x(L); apostrofo(L).
+nelitero(L) --> [L], { 
+         ( L<65, L\=39 ) ; 
+         between(91,96,L) ; 
+         ( L>123 , \+ memberchk(L,[264,265,284,285,308,309,348,349,292,293]) )
+                     }.
+
+%nelitero("") --> tekstofino.
 
 minuskloj([M|V]) --> minusklo(M), minuskloj(V).
 minuskloj([M,X|V]) --> minusklo_x([M,X]), minuskloj(V).
@@ -30,6 +40,10 @@ minuskloj([M,X|V]) --> minusklo_x([M,X]), minuskloj(V).
 minuskloj([A]) --> apostrofo(A).
 minuskloj([M]) --> minusklo(M).
 
+neliteroj([N|Nj]) --> nelitero(N), neliteroj(Nj).
+neliteroj([N]) --> nelitero(N).
+
+% vortoj ...
 vorto(V) --> minuskloj(V).
 vorto([M|V]) --> majusklo(M), minuskloj(V).
 vorto([M,X|V]) --> majusklo_x([M,X]), minuskloj(V).
@@ -37,12 +51,23 @@ vorto([M,X|V]) --> majusklo_x([M,X]), minuskloj(V).
 vortoj([V]) --> vorto(C), { atom_codes(V,C) }.
 vortoj([V|Vj]) --> vorto(C), { atom_codes(V,C) }, spaco, vortoj(Vj).
 
+% teksto kiel alternado de vortoj kaj intersignoj
+vteksto([v(V),s(N)|T]) --> vorto(C), { atom_codes(V,C) }, 
+                     neliteroj(C1), { atom_codes(N,C1) }, vteksto(T).
+vteksto([v(V)]) --> vorto(C), { atom_codes(V,C) }.
+vteksto([]) --> [].
+
+teksto(T) --> vteksto(T).
+teksto([s(N)|T]) --> neliteroj(C), { atom_codes(N,C) }, vteksto(T).
+
+% frazoj ...
 frazeto([Vj,I]) --> vortoj(Vj), intersigno(I).
 
 frazo([[Vj,S]]) --> vortoj(Vj), finsigno(S).
+frazo([[Vj]]) --> vortoj(Vj), tekstofino.
 frazo(F) --> frazeto(F1), spaco, frazo(F2), { append([F1],F2,F) }.
 
-analizo(Frazo) :-
+fraz_analizo(Frazo) :-
   set_prolog_flag(encoding,utf8),
   phrase(frazo(F),Frazo),
   format('~q~n',[F]).
