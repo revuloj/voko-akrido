d3821373a994281a59e56e9af165b38ce57c9484
diestel@steloj.de <diestel@steloj.de@2fe983b5-ae8e-ec12-dd4c-d1d120d9b827>
2015-02-02 19:09:45 +0000

    revo_radikoj.pl mankis

---
+++ revo_radikoj.pl
@@ -0,0 +1,318 @@
+:- use_module(library(sgml)).
+:- use_module(library(xpath)).
+:- use_module(library(semweb/rdf_db)).
+
+:- dynamic radiko/2, evi/2, mlg/1.
+
+revo_xml('/home/revo/revo/xml/*.xml').
+voko_rdf_klasoj('/home/revo/voko/owl/voko.rdf').
+radik_dosiero('vrt/v_revo_radikoj.pl').
+nomo_dosiero('vrt/v_revo_nomoj.pl').
+evi_dosiero('vrt/v_revo_evitindaj.pl').
+mlg_dosiero('vrt/v_revo_mallongigoj.pl').
+
+:- rdf_register_prefix(voko,'http://purl.org/net/voko#').
+
+% kreas faktojn tiajn el Revo: radiko(Radiko,Speco)
+
+info :- format('revo_radikaro :- load_voko_classes, revo_trasercho, skribu.').
+
+%%%%%%%%%%
+% bazaj predikatoj por traserchi artikolojn pri radikoj kaj skribi ilin
+%%%%%%%%%
+
+revo_radikaro :-
+  load_voko_classes, 
+  revo_trasercho, 
+  skribu.
+
+revo_trasercho :-
+  (\+ sub_class(rabobestoj,bestoj) -> throw('chu vi shargis la voko-klasojn jam?'); true),
+
+  retractall(radiko(_,_)),
+  retractall(evi(_,_)),
+  retractall(mlg(_)),
+
+  revo_xml(Xml),
+%  atom_concat(Pado,'/*.xml',XMLDosieroj),
+  expand_file_name(Xml,Dosieroj),
+ 
+  forall(
+      member(Dosiero,Dosieroj),
+      catch(
+        (
+          % format('~w -> ',[Dosiero]),
+          revo_art(Dosiero)
+        ),
+        Exc,
+        handle_exception(Dosiero,Exc)
+      )
+   ).
+
+skribu :-
+  skribu_radikojn,
+  skribu_nomojn,
+  skribu_mallongigojn,
+  skribu_evitindajn.
+
+
+
+skribu_radikojn :-
+  radik_dosiero(Dos),
+  format('skribas al ''~w''...~n',[Dos]),
+  setup_call_cleanup(
+    open(Dos,write,Out),
+    skribu_radikojn(Out),
+    close(Out)
+  ).
+
+skribu_radikojn(Out) :-
+  findall(
+    Rad-Spec,
+    radiko(Rad,Spec),
+    Chiuj),
+  keysort(Chiuj,Ordigitaj),
+  reverse(Ordigitaj,Renversitaj),
+  forall(
+    member(R-S,Renversitaj), % renversu por ke "senil" analiziĝu antaŭ "sen/il" ktp.
+    % format(Out,'r(''~w'',~w) --> "~w".~n',[R,S,R])
+    format(Out,'r(''~w'',~w).~n',[R,S])  
+  ).
+
+skribu_nomojn :-
+  nomo_dosiero(Dos),
+  format('skribas al ''~w''...~n',[Dos]),
+  setup_call_cleanup(
+    open(Dos,write,Out),
+    (
+      skribu_nomojn_maj(Out),
+      skribu_nomojn_min(Out)
+    ),
+    close(Out)
+  ).
+
+skribu_nomojn_maj(Out) :-
+  findall(
+    Rad-Spec,
+    nr(Rad,Spec),
+    Chiuj),
+  keysort(Chiuj,Ordigitaj),
+  reverse(Ordigitaj,Renversitaj),
+  forall(
+    member(R-S,Renversitaj),
+    format(Out,'nr(''~w'',~w).~n',[R,S])  
+  ).
+
+skribu_nomojn_min(Out) :-
+  findall(
+    Rad-Spec,
+    nr_(Rad,Spec),
+    Chiuj),
+  keysort(Chiuj,Ordigitaj),
+  reverse(Ordigitaj,Renversitaj),
+  forall(
+    member(R-S,Renversitaj), 
+    format(Out,'nr_(''~w'',~w).~n',[R,S])  
+  ).
+
+skribu_evitindajn :-
+  evi_dosiero(Dos),
+  format('skribas al ''~w''...~n',[Dos]),
+  setup_call_cleanup(
+    open(Dos,write,Out),
+    skribu_evitindajn(Out),
+    close(Out)
+  ).
+
+skribu_evitindajn(Out) :-
+  forall(
+    evi(Art,Vort),
+    format(Out,'evi(''~w'',"~w").~n',[Art,Vort])
+  ).
+
+skribu_mallongigojn :-
+  mlg_dosiero(Dos),
+  format('skribas al ''~w''...~n',[Dos]),
+  setup_call_cleanup(
+    open(Dos,write,Out),
+    skribu_mallongigojn(Out),
+    close(Out)
+  ).
+
+skribu_mallongigojn(Out) :-
+  forall(
+    mlg(Mlg),
+    format(Out,'mlg(''~w'').~n',[Mlg])
+  ).
+
+load_voko_classes :-
+  rdf_retractall(_,_,_),
+  voko_rdf_klasoj(RdfFile),
+  rdf_load(RdfFile,[]),
+  rdf_set_predicate(rdfs:subClassOf,transitive(true)).
+
+
+%%%%%%%%%%
+% helpaj predikatoj
+%%%%%%%%%
+
+sub_class(X,Class) :-
+%  rdf_global_id(voko:X,VX),
+  rdf_global_id(voko:Class,VClass),
+  rdf_global_id(voko:X,VX),
+  rdf_reachable(VX,rdfs:subClassOf,VClass).
+
+
+handle_exception(Dosiero,Exception) :-
+  once(
+    (
+      Exception = eraro(Eraro), format('~w -> ERARO: ~w~n',[Dosiero,Eraro]);
+      Exception = averto(Averto), format('~w -> AVERTO: ~w~n',[Dosiero,Averto])
+    )
+  ).
+
+
+revo_art(Dosiero) :-
+  load_xml_file(Dosiero,DOM),
+  catch(
+        (
+	 revo_rad(DOM,Radiko,Speco),
+	 revo_mlg(DOM,Mallongigoj)
+       % format('~w (~w)~n',[Radiko,Speco]),
+        ),
+        Exc,
+        (
+          Exc = rad_evi(Kap) 
+           -> assert_evi(Dosiero,Kap), throw(averto('EVI'))
+           ; throw(Exc)
+        )
+     ),
+  once((
+    nomo_majuskla(Radiko),
+    assertz(nr(Radiko,Speco)),
+    assert_nomo_minuskla(Radiko,Speco)
+    ;
+    assertz(radiko(Radiko,Speco))
+  )),
+  assert_mlg(Mallongigoj).
+
+
+assert_nomo_minuskla(Nomo,Speco) :-
+    atom_codes(Nomo,[K|Literoj]),
+    upper_lower(K,M),
+    atom_codes(MNomo,[M|Literoj]),
+    assertz(nr_(MNomo,Speco)).
+
+assert_mlg(Mallongigoj) :-
+    forall(
+      member(Mlg,Mallongigoj),
+      assertz(mlg(Mlg))
+    ).
+	  
+
+assert_evi(Dosiero,Kap) :-
+  % format('assert_evi(''~w'',~w).',[Dosiero,Kap]),
+  once(
+    (
+      % konstruu la kapvorton el radiko kaj finaĵo
+      xpath(Kap,rad(normalize_space),Radiko),
+      xpath(Kap,/self(content),Content),
+      (fin_text(Content,Fin) -> true; Fin=''),
+      atom_concat(Radiko,Fin,Kapvorto),
+      % elprenu la dosieronomon el la pado
+      file_base_name(Dosiero,Dos),
+      sub_atom(Dos,Len,_,_,'.xml'),
+      sub_atom(Dos,_,Len,_,Nomo),
+      % assertu evitindaĵon
+      assertz(evi(Nomo,Kapvorto))
+   )
+ ).
+
+
+revo_rad(DOM,Radiko,Speco) :-
+  xpath(DOM,//art/kap,Kap),
+  xpath(Kap,rad(normalize_space),Radiko),
+  atom_length(Radiko,L), 
+  (L=<1 
+    -> throw(averto('ignoras unuliteran radikon')) % ne akceptu radikojn unuliterajn
+    ; true
+  ),
+  xpath(DOM,//drv(1),Drv),
+  \+ (
+    xpath(Drv,uzo(@tip=stl,text),'EVI'), throw(rad_evi(Kap));
+    xpath(Drv,snc(1)/uzo(@tip=stl,text),'EVI'), throw(rad_evi(Kap))
+  ),
+  once(
+   revo_kls(Drv,Speco);
+   revo_gra(Drv,Speco);
+   revo_fin(Kap,Speco);
+   throw(eraro('speco ne eltrovita'))
+  ). 
+
+revo_kls(Drv,parc) :-
+  xpath(Drv,//ref(@lst),Klaso),
+  Klaso='voko:parencoj'. 
+
+revo_kls(Drv,best) :-
+  xpath(Drv,//ref(@lst),Klaso),
+  atom_concat('voko:',C,Klaso),
+  (sub_class(C,bestoj) ; sub_class(C,personoj)).
+
+revo_gra(Drv,Speco) :-
+  xpath(Drv,//gra/vspec(text),VSpeco),
+  once(
+    VSpeco = ntr -> Speco = ntr;
+    VSpeco = tr -> Speco = tr;
+    VSpeco = x -> Speco = tr;
+    VSpeco = sufikso -> Speco = suf;
+    VSpeco = prefikso -> Speco = pref
+  ).
+
+revo_fin(Kap,Speco) :-
+  xpath(Kap,/self(content),Content),
+  fin_text(Content,Fin),
+  once(
+    Fin = i -> Speco = verb;
+    Fin = o -> Speco = subst;
+    Fin = a -> Speco = adj;
+    Fin = e -> Speco = adv
+  ).
+
+fin_text(Elementoj,Fin) :-
+  once(
+  (
+    member(El,Elementoj), 
+    atom(El), 
+    sub_atom(El,0,1,_,'/'),
+    sub_atom(El,1,1,_,Fin)
+  )).
+
+revo_mlg(DOM,Mallongigoj) :-
+  findall(
+    Mlg,
+    (
+      xpath(DOM,//mlg(normalize_space),Mlg),
+      atom_length(Mlg,L), L>1, nur_majuskloj(Mlg)
+    ),
+    Mallongigoj
+  ).
+
+nomo_majuskla(Nomo) :-
+  atom_codes(Nomo,[L|_]),
+  upper_lower(L,_).
+
+nur_majuskloj(Atom) :-
+  atom(Atom),
+  atom_codes(Atom,Codes),
+  nur_majuskloj(Codes).
+
+nur_majuskloj([]).
+nur_majuskloj([M|Literoj]) :-
+  upper_lower(M,_),
+  nur_majuskloj(Literoj).
+
+    
+test(X) :- 
+ xpath(element(kap, [], ['\n  ', element(rad, [], [abaĵur]), '/o ', element(fnt, [], [element(bib, [], [...])]), '\n']), self, X).
+
+  
