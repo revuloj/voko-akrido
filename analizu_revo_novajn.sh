da0c76ed145aab830a6617d1d22f86f73eef3232
diestel@steloj.de <diestel@steloj.de@2fe983b5-ae8e-ec12-dd4c-d1d120d9b827>
2015-02-03 17:37:29 +0000

    aktualigi chiujn trovo-paghojn post trakto de novaj artikoloj

--- analizu_revo_novajn.sh
+++ analizu_revo_novajn.sh
@@ -5,5 +5,10 @@ plsrc=analizu_revo_xml.pl
 goal=analizu_revo_art_novaj
 #PL=/usr/bin/env swipl
 PL=/usr/local/bin/swipl
+PERL=/usr/bin/perl
 
-exec $PL -q -f "$plsrc" -g "$goal,halt" -t 'halt(1)' --
+# exec 
+$PL -q -f "$plsrc" -g "$goal,halt" -t 'halt(1)' --
+
+$PERL elfiltru_trovojn.perl -k kontrolitaj/ 
+
