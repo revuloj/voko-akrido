18b9ae57da6ed109286cec74bad7308d1dbb2318
diestel@steloj.de <diestel@steloj.de@2fe983b5-ae8e-ec12-dd4c-d1d120d9b827>
2015-03-12 18:07:48 +0000

    korektis nombradon de eraroj, elfiltru ankau post ..prefix.sh

--- trunk/analizu_revo_prefix.sh
+++ trunk/analizu_revo_prefix.sh
@@ -5,5 +5,9 @@ plsrc=analizu_revo_art.pl
 goal="analizu_revo_art_prefix($1)"
 #PL=/usr/bin/env swipl
 PL=/usr/local/bin/swipl
+PERL=/usr/bin/perl
 
-exec $PL -q -f "$plsrc" -g "$goal,halt" -t 'halt(1)' --
+$PL -q -f "$plsrc" -g "$goal,halt" -t 'halt(1)' --
+
+$PERL elfiltru_trovojn.perl -k kontrolitaj/ 
+
