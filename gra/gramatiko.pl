a37326f6c9eaff89a692b1c37996823add736c64
Wolfram Diestel <diestel@steloj.de>
2016-02-27 11:43:00 +0100

    forigis malnovan regulgramatikon favore al automate generitaj regulpredikatoj per term_expnasion

--- gra/gramatiko.pl
+++ gra/gramatiko.pl
@@ -1,37 +1,153 @@
 % PLIBONIGU: anstau uzi user: ebligu importi tion de gramatiko...
 :- op( 1120, xfx, user:(<=) ). % disigas regulo-kapon, de regulesprimo
+:- op( 1120, xfx, user:(<-) ). % disigas regulo-kapon, de esceptesprimo
 :- op( 1110, xfy, user:(~>) ). % enkondukas kondichojn poste aplikatajn al sukcese aplikita regulo
 :- op( 150, fx, user:(&) ). % signas referencon al alia regulo
+%:- op( 500, yfx, user:(~) ). % signas disigindajn vortojn
 
-:- dynamic min_max_len/3, '&'/1.
-:- multifile gra_debug/1.
+:- dynamic min_max_len/3, gra_debug/1.
+:- multifile '&'/1, gra_debug/1.
 
 %%% traduki regulesprimojn al normalaj Prologo-faktoj....
 % 
 term_expansion( RuleHead <= RuleBody , RuleTranslated ) :-
-   RuleHead =.. [RuleName|RuleArgs],
- format('# ~w:~n',[RuleName]),
-   rule_expansion(RuleBody,RuleTerm,RuleCond),
-   RuleTranslated =.. [RuleName,RuleTerm,RuleArgs,RuleCond],
- format('  ~w~n',[RuleTranslated]).
+  format('# ~k ...',[RuleHead]),
+   rule_head(RuleHead,Vrt,Rez,Depth,PredHead),!,
+   once((
+     rule_body(RuleHead,RuleBody,Vrt,Rez,Depth,PredBody)
+     ;  
+       format(atom(Exc),'transformeraro: ~w~n',[RuleHead]), 
+       throw(Exc)
+   )),
+   RuleTranslated = (PredHead :- PredBody),
+  format('bone!~n').
+% format('  ~w~n',[RuleTranslated]).
 
-% regulo kun postkondiĉo
-% regulo: rnomo(Args) <= Exp ~> PK
-% prologo: rnomo(H,RuleTerm,Args,PK)
-rule_expansion(RuleExp ~> PostCond,RuleTerm,[PostCond]) :-
-  rule_expression(RuleExp,RuleTerm),!.
+% esceptoj: <- 
+% rv_sen_fin(e,subst) <- post/e/ul.
+% transformu al: rv_sen_fin(e,subst,posteul,'post/e/ul',_).
+%? au alterantive: rv_sen_fin(e,subst,Vrt,Rez,_Depth) :- Vrt = posteul, Rez = post/e/ul.
 
-% send postkondiĉo
-rule_expansion(RuleExp,RuleTerm,[]) :-
-  rule_expression(RuleExp,RuleTerm).
+term_expansion( RuleHead <- RuleBody , RuleTranslated ) :-
+  format('# ~k ...',[RuleHead]),
+   reduce_full(RuleBody,Flat),
+   RuleHead =..  [RuleName|RuleArgs],
+   append(RuleArgs,[Flat,RuleBody,_Depth],Args),
+   RuleTranslated =.. [RuleName|Args],
+   format('bone!~n').
+% format('  ~w~n',[RuleTranslated]).
 
-% regulesprimo kun du termoj, ekz. p(..) / r(..) aŭ p(..) + &kdrv(...)
-rule_expression(RuleExp,[Op|Refs]) :-
-    RuleExp =.. [Op|Refs],
-    memberchk(Op,['+','/','~','-','*']),!.
 
-% simpla regulesprimo kun nur unu termo, ekz. r(..) aŭ &kdrv(...)
-rule_expression(RuleExp,RuleExp).
+rule_head(RuleHead,Vrt,Rez,Depth,PredHead) :-
+   RuleHead =..  [RuleName|RuleArgs],
+%   RuleArgs = [RuleScheme|_],
+% format('# ~w(~w,_)~n',[RuleName,RuleScheme]),
+   append(RuleArgs,[Vrt,Rez,Depth],Args),
+   PredHead =.. [RuleName|Args].
+
+rule_body(RuleHead,RuleExp ~> PostCond,Vrt,Rez,Depth,PredBody) :-
+  rule_exp(RuleHead,RuleExp,Vrt,Rez,Depth,FirstPart),
+  PredBody =.. [',',FirstPart,PostCond].
+
+rule_body(RuleHead,RuleExp,Vrt,Rez,Depth,PredBody) :-
+  rule_exp(RuleHead,RuleExp,Vrt,Rez,Depth,PredBody).
+
+
+
+rule_exp(RuleHead,RuleExp,Vrt,Rez,Depth,PredExp) :-
+  RuleHead =.. [_,RuleScheme|_],
+  RuleExp =.. [Op|Refs],
+  memberchk(Op,['+','/','~','-','*']),!,
+  Refs = [R1,R2],
+%%%%  Rezulto =.. [Op,Rez1,Rez2], 
+  Rez =.. [Op,Rez1,Rez2], 
+
+  rule_ref(R1,V1,Rez1,D1,RRef1),
+  rule_ref(R2,Rest,Rez2,D1,RRef2),
+  splitter(RuleScheme,R1,R2,Vrt,V1,Rest,Splitter),
+  
+  (memberchk(RuleScheme,['pD','pv','pr','pAP','A+P']) ->
+    Sub = (RRef1,RRef2)
+  ; Sub = (RRef2,RRef1)
+  ),
+
+  PredExp =  (
+   debug(Depth,'?',RuleScheme,Vrt), 
+%     atom_concat(V1,Rest,Vrt), 
+%     V1 \= '', Rest \= '',
+     Splitter,
+     D1 is Depth +1,
+%     RRef2,
+%     RRef1,
+     Sub,
+%%%     Rez=Rezulto,
+   debug(Depth,'*',RuleScheme,Rez) 
+    ).
+%  term_variables(PredExp, [A,B,C,D,E,F]).
+
+rule_exp(_,RuleExp,Vrt,Rez,Depth,PredExp) :-
+  rule_ref(RuleExp,Vrt,Rez,Depth,PredExp).
+
+rule_ref(&RuleRef,Vrt,Rez,Depth,RuleCall) :- !,
+  RuleRef =.. [RuleName|RuleArgs],
+  append([RuleArgs,[Vrt,Rez,Depth]],Args),
+  RuleCall =.. [RuleName|Args].
+
+rule_ref(DictSearch,Vrt,Vrt,_,DictSearch) :-
+  DictSearch =.. [_,Vrt|_].
+
+splitter(RuleScheme,RuleRef1,RuleRef2,Vrt,V1,Rest,Splitter) :-
+    % PLIBONIGU: iom malavantaĝe estas, ke RuleId - unua argumento en RuleRef
+    % ofte estas "_" kaj do ne konata, oni povus rigardi chiujn eblecojn pri specifa regulo
+    % sed ofte elvenas interfvalo 2.. kiu ne multe diferencas de 1..99
+    get_rule_min_max(RuleRef1,Min1,Max1),
+    get_rule_min_max(RuleRef2,Min2,Max2),
+    
+%%%    get_rule_min_max(RuleScheme,MinR,MaxR),
+%    !,
+    once((	
+      memberchk(RuleScheme,['pD','pv','pr','pAP','A+P']),
+        % prefikso pli mallonga ol la resto...
+        Splitter = (
+   %%%       atom_length(Vrt,L),
+   %%%       between(MinR,MaxR,L),
+
+          between(Min1,Max1,L1),
+          sub_atom(Vrt,0,L1,L2,V1),
+
+          between(Min2,Max2,L2),
+          sub_atom(Vrt,L1,L2,0,Rest)
+        )
+/***********
+      ;
+      RuleScheme == 'A+P',
+        Sum1 is Min1 + Max1,
+        Splitter = (
+          % preferu pli longajn radikojn komence...
+          between(Min1,Max1,X1),
+          plus(X1,L1,Sum1),
+          sub_atom(Vrt,0,L1,L2,V1),
+
+          between(Min2,Max2,L2),
+          sub_atom(Vrt,L1,L2,0,Rest)
+        )
+*********/
+      ; % ordinare komencu de malantaue
+       % sufiksoj kaj finaĵoj normale estas mallongaj...
+       Splitter = (
+   %%%     atom_length(Vrt,L),
+   %%%     between(MinR,MaxR,L),
+
+         between(Min2,Max2,L2),
+         sub_atom(Vrt,L1,L2,0,Rest), 
+
+         between(Min1,Max1,L1),
+         sub_atom(Vrt,0,L1,L2,V1)
+     )
+  )).
+
+
+/********************************************************************/
 
 gra_debug(false). % default
 
@@ -46,139 +162,75 @@ debug(Depth,Msg,Scheme,Rezulto) :-
 
 analyze(Vrt,Ana,Spc) :-
   atom(Vrt),
-  apply_rule(&vorto(_,Spc),Vrt,Ana,0).
+  vorto(_,Spc,Vrt,Ana,0).
 
 analyze(Vrt,Ana,Spc) :-
   is_list(Vrt),
   atom_codes(Atom,Vrt),
-  apply_rule(&vorto(_,Spc),Atom,Ana,0).
+  vorto(_,Spc,Atom,Ana,0).
 
-% forigas krampojn kaj spacojn el la rezulo-termo
+analyze_perf(Vrt,Ana,Spc) :-
+  statistics(process_cputime,C1),
+  statistics(inferences,I1),
+  analyze(Vrt,Ana,Spc),
+  statistics(process_cputime,C2),
+  statistics(inferences,I2),
+  I is I2-I1,
+  C is C2-C1,
+  format('inferences: ~w, cpu: ~w~n',[I,C]).
+
+% forigas krampojn kaj spacojn el la rezulto-termo
 reduce(Term,Flat) :-
   format(codes(A),'~w',[Term]),
-  reduce_(A,F),
+  reduce_(A,F,"() "),
   atom_codes(Flat,F).
 
-reduce_([],[]).
-reduce_([L|Ls],[F|Fs]) :-
-  memberchk(L,"() ") 
-   -> reduce_(Ls,[F|Fs])
-   ; F=L, reduce_(Ls,Fs).
+reduce_full(Term,Flat) :-
+  format(codes(A),'~w',[Term]),
+  reduce_(A,F,"/*+~-() "),
+  atom_codes(Flat,F).
 
-% voku ekz: 
-% apply_rule(&vorto(RuleId,Spc),'eniri',Rezulto).
+reduce_([],[],_).
 
-apply_rule(RuleRef,Vrt,Rez) :- apply_rule(RuleRef,Vrt,Rez,0).
+reduce_([L|Ls],F,DelLetters) :-
+  string_code(_,DelLetters,L),!, % memberchk(L,DelLetters),!, 
+  reduce_(Ls,F,DelLetters).
 
-% regulo unuparta
-apply_rule(&RuleRef,Vrt,Rez,Depth) :-
-  % retrovu regulon laŭ la referenco
-  RuleRef =.. [RuleName|Args], 
-  %current_predicate(RuleName/2), 
-  RuleScheme =.. [RuleName,SubRule,Args,Post], 
-  call(RuleScheme), \+ is_list(SubRule),
-  % pli efike, estus, se la regulo nun mem deprenus
-  % vort-parton el la tuta lau la indikoj min_max...
-  % per simpla check nur shparighas la aplikado de la regulo...
-  check_length(Args,Vrt),
-  % apliku la regulesprimon al la vorto
-%  debug(Depth,provas,RuleScheme,Vrt),
-  D1 is Depth+1,
-  apply_rule(SubRule,Vrt,Rez,D1),
-  % apliku postkondiĉon
-  (Post = [Cond] *-> Cond; true),
-  debug(Depth,rezulto,RuleScheme,Rez).
+reduce_([L|Ls],[L|Fs],DelLetters) :-
+  % \+ memberchk(L,"() "), 
+  reduce_(Ls,Fs,DelLetters).
 
-% regulo duparta 
-apply_rule(&RuleRef,Vrt,Rez,Depth) :-
-  % retrovu regulon laŭ la referenco
-  RuleRef =.. [RuleName|Args], 
-  %current_predicate(RuleName/3), 
-  RuleScheme =.. [RuleName,SubRules,Args,Post], 
-  call(RuleScheme), SubRules = [Op|Partoj],
-  % pli efike, estus, se la regulo nun mem deprenus
-  % vort-parton el la tuta lau la indikoj min_max...
-  % per simpla check nur shparighas la aplikado de la regulo...
-  check_length(Args,Vrt),
-  % apliku la regulesprimojn al la vorto
-%  debug(Depth,provas,RuleScheme,Vrt),
-  D1 is Depth+1,
-  apply_prt_rules(Partoj,Vrt,Rezultoj,D1),
-  % apliku postkondiĉon
-  (Post = [Cond] *-> Cond; true),
-  Rez =.. [Op|Rezultoj],
-  debug(Depth,rezulto,RuleScheme,Rez).
-
-% bazaj reguloj referencantaj al vortaro
-apply_rule(DictSearch,Ero,Ero,_) :-
-  DictSearch =.. [Pred,Ero|_], Pred \= '&',
-% pli malrapide tamen, verŝajne valorus
-% limigi la longecon nur che pli supraj regulaplikoj
-% kaj nencesus ankau interrompi, kiam
-% longeco forlasas la [min,max] intervalon
-%  check_length(DictSearch),
-  call(DictSearch).
-
-
-apply_prt_rules([],'',[],_).
-
-apply_prt_rules([Prt|Partoj],Vrt,[Rez|Rezultoj],Depth) :-
-%% ne funkcias, char konkrete regulo ankorau ne elektighis...
-%% oni povus nur kalkuli min, max surbaze de chiuj reguloj kun sama rule_ref
-%% sed tio estas malpli efika
-/**
-    once(get_rule_min_max(Prt,Vrt,Min,Max)),
-    between(Min,Max,L),
-    sub_atom(Vrt,0,L,_,V1),
-    (L = Max -> Partoj = []; true),
-**/
-    atom_concat(V1,Rest,Vrt), V1 \='',
-    % evitu senfinajn ciklojn che maldekstre rikuraj reguloj
-    % (Partoj \= [] -> Rest \= ''; true),
-    (Rest = '' -> Partoj = []; true),
-    % pli efike eble estus havi minimuman kaj maksimuman longecon
-    % kaj uzi between + sub_atom...?
-    apply_rule(Prt,V1,Rez,Depth),
-
-    atom_concat(V1,Rest,Vrt),
-    apply_prt_rules(Partoj,Rest,Rezultoj,Depth).
-
-check_length([RuleId|_],Vrt) :-
-  % kontrolu la longecon de Vrt
-  min_max_len(RuleId,Min,Max) 
-    -> atom_length(Vrt,Len), between(Min,Max,Len)
-    ; true.
-
-check_length(DictSearch) :-
-  % analizetu la serchon
-  DictSearch =.. [Pred,Vrt|Rest],
-  length(Rest,A), Arity is A+1,
-  atom_length(Vrt,Len),
-  % kontrolu la longecon de Vrt
-  get_min_max(Pred/Arity,Min,Max),
-  between(Min,Max,Len).
-
-get_rule_min_max(&RuleRef,Vrt,Min,Max) :-
-  RuleRef =.. [_,RuleId|_],
-  atom_length(Vrt,Len),
-  (nonvar(RuleId), min_max_len(RuleId,Mn,Mx) ->
-     Min is max(1,min(Mn,Len)),
-     Max is min(Mx,Len),
+get_rule_min_max(RuleId,Min,Max) :-
+  atom(RuleId),
+  (min_max_len(RuleId,Mn,Mx) -> 
+     Min is max(1,min(Mn,99)),
+     Max is min(Mx,99),
      debug(0,rmin,RuleId,Min),
      debug(0,rmax,RuleId,Max)
   ; 
      Min is 1, 
-     Max is Len). 
+     Max is 99). 
 
-get_rule_min_max(Search,Vrt,Min,Max) :- 
+get_rule_min_max(&RuleRef,Min,Max) :-
+  RuleRef =.. [_,RuleId|_],
+  (nonvar(RuleId), min_max_len(RuleId,Mn,Mx) -> 
+     Min is max(1,min(Mn,99)),
+     Max is min(Mx,99),
+     debug(0,rmin,RuleId,Min),
+     debug(0,rmax,RuleId,Max)
+  ; 
+     Min is 1, 
+     Max is 99). 
+
+get_rule_min_max(Search,Min,Max) :- 
     Search =.. [Pred|Args],
     length(Args,Arity),
-    atom_length(Vrt,Len),
+%    atom_length(Vrt,Len),
     get_min_max(Pred/Arity,Mn,Mx),
-    Min is max(1,min(Mn,Len)),
-    Max is min(Mx,Len),
-    debug(0,smin,RuleId,Min),
-    debug(0,smax,RuleId,Max).
+    Min is max(1,min(Mn,99)),
+    Max is min(Mx,99),
+    debug(0,smin,Pred,Min),
+    debug(0,smax,Pred,Max).
 
 get_min_max(Pred/Arity,Min,Max) :-
   min_max_len(Pred/Arity,Min,Max) -> true
@@ -191,8 +243,3 @@ get_min_max(Pred/Arity,Min,Max) :-
     max_list(Lens,Max),
     min_list(Lens,Min),
     assert(min_max_len(Pred/Arity,Min,Max)).
-
-    
-
-
-
