66776fc4653bb4a72716dc4a832a9164ef6b7ff0
Wolfram Diestel <diestel@steloj.de>
2018-12-19 17:34:48 +0100

    elshuti XML-dosierojn kiel arhhivo kaj transformi ilin al txt

---
+++ process_xml_archive.sh
@@ -0,0 +1,26 @@
+zip=$1
+lynx="/usr/bin/lynx -nolist -dump -assume_local_charset=utf8 -display_charset=utf8 -stdin"
+xsltproc=/usr/bin/xsltproc
+XSL=xsl/revotxt_eo.xsl
+time=`date +%Y%m%d_%H%M%S`
+
+echo "malpakante $zip ..."
+unzip -ju ${zip} -d xml/ "revo/xml/*.xml"
+
+echo "tradukante ĉiujn pli novajn dosierojn el XML al TXT ..."
+for src in xml/*.xml; do
+    
+    # konstruu nomon de la txt-dosiero
+    suffix=${src#xml/}
+    file=${suffix%%.xml}
+    trg=txt/${file}.txt
+
+    if [ "$src" -nt "$trg" ]; then
+        printf '%s\n' "$src -> $trg"
+        echo "$xsltproc $XSL $src 2>> tmp/${time}_x2t.log | $lynx > $trg"
+        $xsltproc $XSL $src 2>> tmp/${time}_x2t.log | $lynx > $trg
+    fi
+
+done
+
+
