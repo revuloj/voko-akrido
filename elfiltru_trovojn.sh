2727652d632f61b4af0e8540afbbe02f467fd7a4
Wolfram Diestel <diestel@steloj.de>
2018-12-21 17:42:14 +0100

    aldonis nur-tekstan elfiltron

--- elfiltru_trovojn.sh
+++ elfiltru_trovojn.sh
@@ -3,6 +3,7 @@
 function elfiltru {
   d=$1
   outformat=$2
+  revoart="http://retavortaro.de/art/"
 
   #suffix=${d#html/}
 
@@ -22,52 +23,44 @@ function elfiltru {
           Por prijuĝi ilin <a href="klarigoj.html">vidu ankaŭ la klarigojn</a>.
         </p>
 EOH
+
+    echo "elfiltru en $d..."
+    # trovi dubindajn/erarajn vortojn, forigu duoblajn kaj grupigu laŭ dosiernomo
+    grep -oH "<span class=.*</span>" "$d"/* | sed 's/.*html\///' | awk '!a[$0]++' - | \
+      awk -F":" '{a[$1]=a[$1] ? a[$1]", "$2 : $2} END \
+      {for (i in a) {print "<a href=\47"i"\47>"i"</a> "\
+      "<a href=\47'$revoart'"substr(i,3)"\47 class=\47redakti\47 target=\47_new\47\
+      title=\47artikolo\47>&#x270E;</a>: "a[i]"<br>"}}' - \
+      >> "$outfile"
+    # $ sort -u input.txt
+    # $ awk '!a[$0]++' input.txt    
+    
+    # grouping with awk: https://www.thelinuxrain.com/articles/grouping-with-awk
+
+    cnt_artikoloj=`grep -l "<span class=.*</span>" "$d"/* | wc -l`
+
+    cat <<EOF >> "$outfile"
+          <p>
+            artikoloj kun trovoj: $cnt_artikoloj
+          </p>
+        </body>
+      </html>
+EOF
+
+  # nur-teksta...
   else
     # malplena...
     outfile="${d}_trovoj.txt"
     echo "skribante al $outfile"
     echo "" > "$outfile"
-  fi
 
-  echo "elfiltru en $d..."
-  # trovi dubindajn/erarajn vortojn, forigu duoblajn kaj grupigu laŭ dosiernomo
-  grep -oH "<span class=.*</span>" "$d"/* | sed 's/.*html\///' | awk '!a[$0]++' - | \
-    awk -F":" '{a[$1]=a[$1] ? a[$1]", "$2 : $2} END {for (i in a) {print i": "a[i]}}' - \
+    # forigu prefiksan ujon, html-strukturilojn kaj duoblaĵojn
+    # provizore ne grupigu laŭ dosiero
+    grep -oH "<span class=.*</span>" "$d"/* | sed 's/.*html\///' |\
+      sed 's/<span[^>]*>//' | sed 's/<\/span>//' | awk '!a[$0]++' - \
     >> "$outfile"
-  # $ sort -u input.txt
-  # $ awk '!a[$0]++' input.txt    
-  
-  # grouping with awk: https://www.thelinuxrain.com/articles/grouping-with-awk
 
-  cnt_artikoloj=`grep -l "<span class=.*</span>" "$d"/* | wc -l`
-
-  if [ $outformat == "html" ]; then
-  	cat <<EOF >> "$outfile"
-        <p>
-          artikoloj kun trovoj: $cnt_artikoloj
-        </p>
-      </body>
-    </html>
-EOF
   fi
-
-#  	print "<a href='$file'>$fileref</a> ";
-#		print "<a href='$REVO/art/$fileref.html' class='redakti' ";
-		#print "title='artikolo' target='_new'>&#x270E;</a>: ";
-
-#		print join(', ',@eraroj2);
-#		print "<br/>\n";
-#	    } else {
-#		print "$fileref: ";
-#		my @err = map { m|>(.*?)</span|; $1 } @eraroj2;
-#		print join(', ',@err);
-#		print "\n";
-#	    }
-
-#           $cnt++;
-
-
-
 }  
 
 for dir in html/[a-z]
