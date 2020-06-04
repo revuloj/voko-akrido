bb70768c5637d0f45d1e60637e4f88038a50a62c
Wolfram Diestel <wolfram.diestel@t-systems.com>
2020-06-04 17:08:02 +0200

    plibonigis romian nombro-gramatikon, sed kunmeti ekz X+IV ankoraŭ ne eblas.

--- pro/dcg/romiaj_dcg.pl
+++ pro/dcg/romiaj_dcg.pl
@@ -1,22 +1,32 @@
 %encoding('utf8').
 
-% ciferoj, t.e. nombroj de 1 ghis 9
+% cj, t.e. nombroj de 1 ghis 9
 
-cifero(10) --> "X".
-cifero(5) --> "V".
-cifero(1) --> "I".
+c1(10) --> "X".
+c1(1) --> "I".
+
+c5(5) --> "V".
+
+% ciferoj
+c(N) --> c1(N).
+c(N) --> c5(N).
+
+% tiuj rajtas lokigi maldekstre
+%c(N) --> c1(N).
 
 % malpermesu VV, LL, DD...!
 
-% kombinoj de pli ol du ciferoj...
+% kombinoj de pli ol du cj...
 
-nombro(N) --> cifero(N).
-nombro(NN) --> cifero(N), cifero(N), { NN is N+N }.
-nombro(NNN) --> cifero(N), cifero(N), cifero(N), { NNN is N+N+N }.
-nombro(MN) --> cifero(M), cifero(N), { M<N, MN is N-M }.
-nombro(NM) --> cifero(N), cifero(M), { N>M, NM is N+M }.
-nombro(NMM) --> cifero(N), cifero(M), cifero(M), { N>M, NMM is N+M+M }.
-nombro(NMMM) --> cifero(N), cifero(M), cifero(M), cifero(M), { N>M, NMMM is N+M+M+M }.
+nombro(N) --> c(N).
+nombro(NN) --> c1(N), c1(N), { NN is N+N }.
+nombro(NNN) --> c1(N), c1(N), c1(N), { NNN is N+N+N }.
+
+nombro(MN) --> c1(M), c(N), { M<N, MN is N-M }.
+
+nombro(NM) --> c(N), c(M), { N>M, NM is N+M }.
+nombro(NMM) --> c(N), c1(M), c1(M), { N>M, NMM is N+M+M }.
+nombro(NMMM) --> c(N), c1(M), c1(M), c1(M), { N>M, NMMM is N+M+M+M }.
 
 
 % kunmetu ambau direktojn en unu predikato
