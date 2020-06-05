a2cc36c9eb1c3f1dde9ba02eb5f99e87114eee77
Wolfram Diestel <wolfram.diestel@t-systems.com>
2020-06-05 09:45:49 +0200

    gramatiko de romiaj nombroj funkcias nun

--- pro/dcg/romiaj_dcg.pl
+++ pro/dcg/romiaj_dcg.pl
@@ -1,22 +1,28 @@
 %encoding('utf8').
 
-% cj, t.e. nombroj de 1 ghis 9
+% bazaj ciferoj
 
-c1(10) --> "X".
 c1(1) --> "I".
+c1(10) --> "X".
+c1(100) --> "C".
+c1(1000) --> "M".
 
-c5(50) --> "L".
 c5(5) --> "V".
+c5(50) --> "L".
+c5(500) --> "D".
 
 % ciferoj
 %c(N) --> c1(N).
 %c(N) --> c5(N).
 
-% II, III, XX, XXX ktp.
+% adicio: II, III, XX, XXX ktp.
 a(NN) --> c1(N), c1(N), { NN is N+N }.
 a(NNN) --> c1(N), c1(N), c1(N), { NNN is N+N+N }.
+a(NM) --> c5(N), c1(M), { N is M*5, NM is N+M }. % ekz. VI
+a(NMM) --> c5(N), c1(M), c1(M), { N is M*5, NMM is N+M+M }. % ekz. VII
+a(NMMM) --> c5(N), c1(M), c1(M), c1(M), { N is M*5, NMMM is N+M+M+M }. % ekz. VIII
 
-% 4 (IV), 9 (IX), 40 (XL), 90 (XC), 400 (CD), 900 (CM)
+% subtraho: 4 (IV), 9 (IX), 40 (XL), 90 (XC), 400 (CD), 900 (CM)
 s(MN) --> c1(M), c5(N), { N is M*5, MN is N-M }.
 s(MN) --> c1(M), c1(N), { N is M*10, MN is N-M }.
 
@@ -25,22 +31,50 @@ n(N) --> c5(N).
 n(N) --> a(N).
 n(N) --> s(N).
 
-nombro(N_)  --> n(N_).
-nombro(N_M_) --> n(N_), n(M_), { N_>M_, N_M_ is N_+M_ }.
-nombro(N_M_L_) --> n(N_), n(M_), n(L_), { N_>M_, M_>L_, N_M_L_ is N_+M_+L_ }.
+%nombro(N_)  --> n(N_).
+%nombro(N_M_) --> nombro(N_), ",", n(M_), { N_>M_, N_M_ is N_+M_ }.
+
+%% decimala dismeto
+%rn(D) --> n(D), { D =< 10 }.
+%rn(D) --> n(D10), { D10 is div(D,10)*10, 0 is D mod 10 }.
+%rn(D) --> n(D10), n(D1), { D10 is div(D,10)*10, D1 is D mod 10 }.
+romia(Dec,Rom) :-
+  number_chars(Dec,CList),
+  ndec(CList,DList),
+  rnl(DList,RList),
+  append(RList,RL1),
+  atom_codes(Rom,RL1).
+
+ndec(CL,DL) :- ndec(CL,_,DL).
+ndec([C],1,[D]):- atom_number(C,D),!.
+ndec([C|CL],P10,[D|DL]) :-
+  ndec(CL,P,DL),
+  P10 is P*10,
+  atom_number(C,N),
+  D is P10 * N.
+
+rnl([],[]).
+rnl([D|DList],[R|RList]) :-
+  D > 0,
+  phrase(n(D),R),!,
+  rnl(DList,RList).
+rnl([D|DList],RList) :-
+  D = 0,
+  rnl(DList,RList).
+
 
 % kunmetu ambau direktojn en unu predikato
 
-nombro(Nombro,N) :-
-  integer(N), phrase(nombro(N),Codes), atom_codes(Nombro,Codes),!;
-  atom(Nombro), atom_codes(Nombro,Codes), phrase(nombro(N),Codes),!.
+%romia(Nombro,N) :-
+%  integer(N), phrase(rn(N,Codes), atom_codes(Nombro,Codes),!;
+%  atom(Nombro), atom_codes(Nombro,Codes), phrase(rn(N),Codes),!.
 
 % nombri 
 
 nombru(De,Ghis) :-
   between(De,Ghis,N),
-  nombro(Nombro,N),
-  write(Nombro),nl,fail.
+  romia(N,Romia),
+  write(Romia),nl,fail.
 
 
 
