ee89964d5f0a4950e16f69db68c22a6831db4dfd
Wolfram Diestel <wolfram.diestel@t-systems.com>
2020-12-16 17:01:46 +0100

    skriptojn sovis al bin/

--- bin/revo_radikoj.sh
+++ bin/revo_radikoj.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # base=<absolute-path-to-source>
-plsrc=revo_radikoj.pl
+plsrc=pro/revo_radikoj.pl
 goal=revo_radikaro
 #PL=/usr/bin/env swipl
 PL=/usr/bin/swipl
 
-cd pro
+#cd pro
 $PL -q -f "$plsrc" -g "$goal" -t halt --
-cd ..
+#cd ..
 
