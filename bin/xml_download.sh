ac5ab9c767e4bfccb7ba1bdf94a5f6bf488a1473
Wolfram Diestel <diestel@steloj.de>
2020-12-28 17:26:20 +0100

    foriug xml/* antaŭ maslpaki por ne konservi jam forigitajn dosierojn

--- bin/xml_download.sh
+++ bin/xml_download.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 #set -e
 
@@ -7,7 +6,10 @@ xml_url=https://github.com/revuloj/revo-fonto/archive/master.zip
 echo "master.zip <- ${xml_url}"
 curl -L -H "Accept: application/zip" -o master.zip "${xml_url}"
 
-# -j = no dirs, -q = quiet, -o = overwrite existing, -u update as needed
-unzip -juqo master.zip -d xml/ "revo-fonto-master/revo/*.xml" \
+# ni forigas dosierojn en xml por esti certa, ne konsideri malnovajn doseriojn
+rm xml/*
+
+# -j = no dirs, -q = quiet, -o = overwrite existing, -u update as needed (unzip -juqo...)
+unzip -jq master.zip -d xml/ "revo-fonto-master/revo/*.xml" \
   && rm -rf revo-fonto-master && rm master.zip
 
