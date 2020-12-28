1e14680630342fa1ab2bcaeb2b830deee774211e
Wolfram Diestel <diestel@steloj.de>
2020-12-28 17:26:36 +0100

    permesu prefike nur konverti certajn

--- bin/xml2txt.sh
+++ bin/xml2txt.sh
@@ -1,18 +1,29 @@
+#!/bin/bash
+
+# Vi povas doni prefikson kiel unua argumento: xml2txt.sh k
+# tiam konvertiĝas nur xml/k*.xml, se ili estas pli novaj ol 
+# la korespondaj txt/k*.xml
+# Se vi volas aktualigi ĉiujn, voku sen argumento.
+
 lynx="/usr/bin/lynx -nolist -dump -assume_local_charset=utf8 -display_charset=utf8 -stdin"
 xsltproc=/usr/bin/xsltproc
 XSL=xsl/revotxt_eo.xsl
 time=`date +%Y%m%d_%H%M%S`
 
+ls xml/${1}*.xml
+
 echo "tradukante ĉiujn pli novajn dosierojn el XML al TXT ... (daŭras iom...)"
-for src in xml/*.xml; do
+for src in xml/${1}*.xml; do
     
     # konstruu nomon de la txt-dosiero
     suffix=${src#xml/}
     file=${suffix%%.xml}
     trg=txt/${file}.txt
 
+    #printf '%s\n' "$src -> $trg ?"
+
     if [ "$src" -nt "$trg" ]; then
-        # printf '%s\n' "$src -> $trg"
+        printf '%s\n' "$src -> $trg"
         # echo "$xsltproc $XSL $src 2>> tmp/${time}_x2t.log | $lynx > $trg"
         $xsltproc $XSL $src 2>> tmp/${time}_x2t.log | $lynx > $trg
     fi
