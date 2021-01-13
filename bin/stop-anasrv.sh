3f818b70e45431032b9e042c67aa1c5571cc0d89
Wolfram Diestel <diestel@steloj.de>
2021-01-13 17:41:00 +0100

    adaptoj de analizo-servo: renomo al akrido, adapto de padoj

--- bin/stop-anasrv.sh
+++ bin/stop-anasrv.sh
@@ -4,13 +4,13 @@
 # kiel servo, necesas iom da manipulado por eltrovi
 # la uzanton (ordinare revo aŭ revo-test)
 
-# eltrovu absolutan padon de tiu ĉi skripto
+# eltrovu uzanto-nomon kaj PID-dosieron
 script=$(readlink -f "$BASH_SOURCE")
 base=$(dirname "$script")
 
 suffix=${base#/home/}
 user=${suffix%%/*}
 
-pidfile=/var/lock/swi.analizilo.${user}
+pidfile=/var/lock/swi.akrido.${user}
 
 /sbin/start-stop-daemon --stop --retry=TERM/30/KILL/5 --pidfile $pidfile --name swipl
